0bc885aaccaf77eb3d85f6974ebabf6d24b43bd2
Zeck-Garcia <henriquegarcia-hg@hotmail.com>
2023-08-29 08:22:06 +0100

    edit

--- app/views/pages/aside/info-despesa-geral.php
+++ app/views/pages/aside/info-despesa-geral.php
@@ -1,6 +1,52 @@
-</php
+<?php
+    $hoje = date("m/Y");
 
+    //INICIO FILTER RESUMO TITULAR
+    $sqlFilterResumoTitular = "SELECT DISTINCT titularReceita, SUM(valorReceita) AS valorReceita, dataReceita FROM tbreceita WHERE dataReceita IN
+    (    
+        SELECT dataReceita valorReceita FROM tbreceita WHERE DATE_FORMAT(dataReceita, '%m/%Y')='$hoje'
+    )
+    GROUP BY titularReceita
+    ";
+    $qryFilterResumoTitular = $operation->executarSQL($sqlFilterResumoTitular);
+    //FIM FILTER RESUMO TITULAR
 
+    //INICIO FILTER SALDO MES ATUAL
+    $sqlFilterSaldoMesAtual = "SELECT dataReceita, SUM(valorReceita) AS valorReceita FROM tbreceita WHERE DATE_FORMAT(dataReceita, '%m/%Y')='$hoje'
+    ";
+    $qryFilterSaldoMesAtual = $operation->executarSQL($sqlFilterSaldoMesAtual);
+
+    while($resumoSaldoMesAtual = $operation->listar($qryFilterSaldoMesAtual))
+    { 
+         $saldoMesAtual = $resumoSaldoMesAtual["valorReceita"];
+    }//FIM FILTER SALDO MES ATUAL
+
+    //INICIO FILTER DIVIDA MES ATUAL
+    $sqlFilterDividaMesAtual = "SELECT SUM(valorDespesaDescricao) AS valorDespesaDescricao FROM tbdespesadescricao WHERE DATE_FORMAT(dataPagamentoDespesaDescricao, '%m/%Y')='$hoje'
+    ";
+    
+    $qryFilterDividaMesAtual = $operation->executarSQL($sqlFilterDividaMesAtual);
+
+    while($resumoDividaMesAtual = $operation->listar($qryFilterDividaMesAtual))
+    { 
+       $dividaMesAtual = number_format($resumoDividaMesAtual["valorDespesaDescricao"], 2, ",", ".");
+    }//FIM FILTER DIVIDA MES ATUAL
+
+    //INICIO FILTER A PAGAR MES ATUAL
+    $sqlFilterAPagarMesAtual = "SELECT situacaoDespesaDescricao, SUM(valorDespesaDescricao) AS valorDespesaDescricao FROM tbdespesadescricao WHERE DATE_FORMAT(dataPagamentoDespesaDescricao, '%m/%Y')='$hoje' AND situacaoDespesaDescricao<>'pago'
+    ";
+    
+    $qryFilterAPagarMesAtual = $operation->executarSQL($sqlFilterAPagarMesAtual);
+    
+    while($resumoAPagarMesAtual = $operation->listar($qryFilterAPagarMesAtual))
+    { 
+         $aPagarMesAtual = number_format($resumoAPagarMesAtual["valorDespesaDescricao"], 2, ",", ".");
+    }//FIM FILTER A PAGAR MES ATUAL
+
+   $totalA = ($saldoMesAtual == "" ? 100 : $saldoMesAtual);
+//    $total = is_numeric(($aPagarMesAtual) - is_numeric($saldoMesAtual));
+
+   echo bcadd($totalA, 50);
 ?>
 
 <div class="row p-3">
@@ -12,7 +58,7 @@
             <h6 class="card-subtitle mb-2 text-muted">saldo inicial da conta no mes atual</h6>
             <div class="row ">
                 <ul class="list-group">
-                    <li class="card-title list-group-item text-center bg-success text-white"><h4><span class="text-white"><?= $moeda . " "?></span>1.965,00</h4></li>
+                    <li class="card-title list-group-item text-center bg-success text-white"><h4><span class="text-white"><?= $moeda . " "?></span><?= $saldoMesAtual ?></h4></li>
                 </ul>
             </div>            
             
@@ -22,7 +68,7 @@
             <h6 class="card-subtitle mb-2 text-muted">total da divida desse mes</h6>
             <div class="row">
                 <ul class="list-group">
-                    <li class="card-title list-group-item text-center bg-info text-white"><h4><span class="text-white"><?= $moeda . " "?></span>1.965,00</h4></li>
+                    <li class="card-title list-group-item text-center bg-info text-white"><h4><span class="text-white"><?= $moeda . " "?></span><?= $dividaMesAtual?></h4></li>
                 </ul>
             </div>  
         </div>
@@ -31,7 +77,7 @@
             <h6 class="card-subtitle mb-2 text-muted">a ainda a pagar</h6>
             <div class="row">
                 <ul class="list-group">
-                    <li class="card-title list-group-item text-center bg-warning text-white"><h4><span class="text-white"><?= $moeda . " "?></span>1.965,00</h4></li>
+                    <li class="card-title list-group-item text-center bg-warning text-white"><h4><span class="text-white"><?= $moeda . " "?></span><?= $aPagarMesAtual?></h4></li>
                 </ul>
             </div>  
         </div>
@@ -40,7 +86,7 @@
             <h6 class="card-subtitle mb-2 text-muted">saldo da conta após pagar todas as contas</h6>
             <div class="row">
                 <ul class="list-group">
-                    <li class="card-title list-group-item text-center bg-secondary text-white"><h4><span class="text-white"><?= $moeda . " "?></span>1.965,00</h4></li>
+                    <li class="card-title list-group-item text-center bg-secondary text-white"><h4><span class="text-white"><?= $moeda . " "?></span></h4></li>
                 </ul>
             </div>  
         </div>
@@ -48,17 +94,18 @@
         <div class="row shadow p-3 mb-2 bg-white rounded">
             
             <?php
-                $sqlFilterResumoTitular = "SELECT * FROM tbreceita";
-                
-                $qryFilterResumoTitular = $operation->executarSQL($sqlFilterResumoTitular);
-                
+                // $date = new DateTime($resumoTitular["dataReceita"]); 
+                // $date->format("m/Y");
+
                 while($resumoTitular = $operation->listar($qryFilterResumoTitular))
-                { ?>
+                { 
+                    ?>
                     <details class="card mt-3">
                     <!-- echo $resumoTitular["titularReceita"]; -->
-                    <summary class="card-header"><?= $resumoTitular["titularReceita"]?></summary>
+                    <summary class="card-header"><?= $resumoTitular["titularReceita"] ?></summary>
                 <ul class="card-body">
-                    <li class="list-group-item"><?="Salário ".$moeda. number_format($resumoTitular["valorReceita"], 2, ",", ".") ?></li>
+                    <li class="list-group-item"><?= "Salário: ".$moeda. number_format($resumoTitular["valorReceita"], 2, ",", ".") ?></li>
+                    <!-- <li class="list-group-item"></li> -->
                     <!-- <li class="list-group-item">valor já pago</li> -->
                     <!-- <li class="list-group-item">saldo da conta</li> -->
                     <!-- <li class="list-group-item">a pagar</li> -->
@@ -67,7 +114,6 @@
                     <!-- <li class="list-group-item"></li> -->
                 </ul>
             </details>
-                
                 
                 <?php }
             ?>
